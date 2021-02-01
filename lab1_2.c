413b07e2d58be53af144fbface7e84ae948ece10
b6320502509 <sasicha.sal@ku.th>
2021-02-01 18:05:17 +0700

    Create lab1_2.c

---
+++ lab1_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter A : ");
+    scanf("%d",&a);
+    printf("Enter B : ");
+    scanf("%d",&b);
+    printf("%d",a+b);
+    return 0;
+}
